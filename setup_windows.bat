c0926bb7495f22ce3f59bdc4281d60cfa8a5c2e3
rogerrgab <gabrielroger2024@protonmail.com>
2026-01-27 02:12:38 -0300

    Fix:Ajustando possíveis erros do 'ven' na instação Windows

--- setup_windows.bat
+++ setup_windows.bat
@@ -1,17 +1,31 @@
 @echo off
 echo 🚀 Iniciando setup do DataSchoolAnalyzer para Windows...
 
-:: 1. Instala dependências
-echo 📦 Instalando bibliotecas...
+:: Criando ambiente virtual se n existir
+if not exist venv (
+    echo 🐍 Criando ambiente virtual...
+    python -m venv venv
+)
+
+:: Ativando ambiente virtual
+echo 🔌 Ativando ambiente virtual...
+call venv\Scripts\activate
+
+:: Garantindo q estamos usando o pip do venv p/ instalar e atualizar
+echo 📦 Instalando/Atualizando dependências...
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-:: 2. Executa o Pipeline
+:: Executando scripts usando o python do venv de forma explícita
 echo 🗄️ Populando o Banco de Dados...
-python scripts/popular_banco.py
+python scripts\popular_banco.py
 
 echo ⚙️ Executando Transformação (ETL)...
-python scripts/analise_escola.py
+python scripts\analise_escola.py
 
+:: Executando Streamlit
 echo 📊 Abrindo o Dashboard...
-streamlit run scripts/dashboard.py
+:: Usando 'python -m streamlit' para garantir que ele use o pacote instalado no venv
+python -m streamlit run scripts\dashboard.py
+
 pause
